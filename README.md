bc6a9726365afa89db666f4e19a53ffd4d29be49
alkeshraj1 <k191302@nu.edu.pk>
2023-04-29 14:42:14 +0500

    Classwork8

---
+++ README.md
@@ -0,0 +1,3 @@
+# SMD Classwork 8
+
+Group Members: 19k1042 Abdullah Abid, 19k1040 Maria Mehmood, 19k0186 Imad Mehmood
---
+++ lib/posts/posts.dart
@@ -0,0 +1,6 @@
+export 'view/posts_page.dart';
+export 'view/posts_list.dart';
+export 'view/view.dart';
+export 'models/post.dart';
+export 'bloc/post_bloc.dart';
+export 'widgets/widgets.dart';
